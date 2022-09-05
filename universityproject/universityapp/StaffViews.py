285870ca54969def55beb54dbf64a68a2048a8b5
Nimya Nampoothiri <nimyanampoothiri@Nimyas-MacBook-Air.local>
2022-09-05 22:14:00 +0530

    University Project

---
+++ universityproject/universityapp/StaffViews.py
@@ -0,0 +1,149 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+from universityapp.models import CustomUser, Staffs, Courses, Subjects, Students, SessionYearModel, \
+    StudentResult
+
+
+def staff_home(request):
+    # Fetching All Students under Staff
+
+    subjects = Subjects.objects.filter(staff_id=request.user.id)
+    course_id_list = []
+    for subject in subjects:
+        course = Courses.objects.get(id=subject.course_id.id)
+        course_id_list.append(course.id)
+
+    final_course = []
+    # Removing Duplicate Course Id
+    for course_id in course_id_list:
+        if course_id not in final_course:
+            final_course.append(course_id)
+
+    students_count = Students.objects.filter(course_id__in=final_course).count()
+    subject_count = subjects.count()
+
+    subject_list = []
+    # # attendance_list = []
+    for subject in subjects:
+        subject_list.append(subject.subject_name)
+        student_list = []
+    context = {
+        "students_count": students_count,
+        "subject_count": subject_count,
+        "subject_list": subject_list,
+        "student_list": student_list,
+    }
+    return render(request, "staff_template/staff_home_template.html", context)
+
+
+# WE don't need csrf_token when using Ajax
+@csrf_exempt
+def get_students(request):
+    # Getting Values from Ajax POST 'Fetch Student'
+    subject_id = request.POST.get("subject")
+    session_year = request.POST.get("session_year")
+
+    # Students enroll to Course, Course has Subjects
+    # Getting all data from subject model based on subject_id
+    subject_model = Subjects.objects.get(id=subject_id)
+
+    session_model = SessionYearModel.objects.get(id=session_year)
+
+    students = Students.objects.filter(course_id=subject_model.course_id, session_year_id=session_model)
+
+    # Only Passing Student Id and Student Name Only
+    list_data = []
+
+    for student in students:
+        data_small = {"id": student.admin.id, "name": student.admin.first_name + " " + student.admin.last_name}
+        list_data.append(data_small)
+
+    return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
+
+
+def staff_profile(request):
+    user = CustomUser.objects.get(id=request.user.id)
+    staff = Staffs.objects.get(admin=user)
+
+    context = {
+        "user": user,
+        "staff": staff
+    }
+    return render(request, 'staff_template/staff_profile.html', context)
+
+
+def staff_profile_update(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method!")
+        return redirect('staff_profile')
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+
+        try:
+            customuser = CustomUser.objects.get(id=request.user.id)
+            customuser.first_name = first_name
+            customuser.last_name = last_name
+            if password != None and password != "":
+                customuser.set_password(password)
+            customuser.save()
+
+            staff = Staffs.objects.get(admin=customuser.id)
+            staff.address = address
+            staff.save()
+
+            messages.success(request, "Profile Updated Successfully")
+            return redirect('staff_profile')
+        except:
+            messages.error(request, "Failed to Update Profile")
+            return redirect('staff_profile')
+
+
+def staff_add_result(request):
+    subjects = Subjects.objects.filter(staff_id=request.user.id)
+    session_years = SessionYearModel.objects.all()
+    context = {
+        "subjects": subjects,
+        "session_years": session_years,
+    }
+    return render(request, "staff_template/add_result_template.html", context)
+
+
+def staff_add_result_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method")
+        return redirect('staff_add_result')
+    else:
+        student_admin_id = request.POST.get('student_list')
+        assignment_marks = request.POST.get('assignment_marks')
+        exam_marks = request.POST.get('exam_marks')
+        subject_id = request.POST.get('subject')
+
+        student_obj = Students.objects.get(admin=student_admin_id)
+        subject_obj = Subjects.objects.get(id=subject_id)
+
+        try:
+            # Check if Students Result Already Exists or not
+            check_exist = StudentResult.objects.filter(subject_id=subject_obj, student_id=student_obj).exists()
+            if check_exist:
+                result = StudentResult.objects.get(subject_id=subject_obj, student_id=student_obj)
+                result.subject_assignment_marks = assignment_marks
+                result.subject_exam_marks = exam_marks
+                result.save()
+                messages.success(request, "Result Updated Successfully!")
+                return redirect('staff_add_result')
+            else:
+                result = StudentResult(student_id=student_obj, subject_id=subject_obj, subject_exam_marks=exam_marks,
+                                       subject_assignment_marks=assignment_marks)
+                result.save()
+                messages.success(request, "Result Added Successfully!")
+                return redirect('staff_add_result')
+        except:
+            messages.error(request, "Failed to Add Result!")
+            return redirect('staff_add_result')
---
+++ universityproject/universityapp/StudentViews.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage  # To upload Profile Picture
+from django.urls import reverse
+import datetime  # To Parse input DateTime into Python Date Time Object
+
+from universityapp.models import CustomUser, Staffs, Courses, Subjects, Students, StudentResult
+
+
+def student_home(request):
+    student_obj = Students.objects.get(admin=request.user.id)
+    course_obj = Courses.objects.get(id=student_obj.course_id.id)
+    total_subjects = Subjects.objects.filter(course_id=course_obj).count()
+
+    subject_name = []
+    subject_data = Subjects.objects.filter(course_id=student_obj.course_id)
+    for subject in subject_data:
+        subject_name.append(subject.subject_name)
+
+    context = {
+        "total_subjects": total_subjects,
+        "subject_name": subject_name,
+
+    }
+    return render(request, "student_template/student_home_template.html", context)
+
+
+def student_profile(request):
+    user = CustomUser.objects.get(id=request.user.id)
+    student = Students.objects.get(admin=user)
+
+    context = {
+        "user": user,
+        "student": student
+    }
+    return render(request, 'student_template/student_profile.html', context)
+
+
+def student_profile_update(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method!")
+        return redirect('student_profile')
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+
+        try:
+            customuser = CustomUser.objects.get(id=request.user.id)
+            customuser.first_name = first_name
+            customuser.last_name = last_name
+            if password != None and password != "":
+                customuser.set_password(password)
+            customuser.save()
+
+            student = Students.objects.get(admin=customuser.id)
+            student.address = address
+            student.save()
+
+            messages.success(request, "Profile Updated Successfully")
+            return redirect('student_profile')
+        except:
+            messages.error(request, "Failed to Update Profile")
+            return redirect('student_profile')
+
+
+def student_view_result(request):
+    student = Students.objects.get(admin=request.user.id)
+    student_result = StudentResult.objects.filter(student_id=student.id)
+    context = {
+        "student_result": student_result,
+    }
+    return render(request, "student_template/student_view_result.html", context)
